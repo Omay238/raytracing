c6002a8a0dba898ced612d199329749064497068
Omay238 <238maculo238@gmail.com>
2026-01-03 16:10:01 -0700

    part 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use libpbm::NetPBM;
+
 fn main() {
-    println!("Hello, world!");
+    // Image
+
+    let image_width = 256;
+    let image_height = 256;
+
+    // Render
+
+    let mut img = NetPBM::new_ppm(image_width, image_height, 255);
+
+    for j in 0..image_height {
+        println!("Scanlines remaining: {}", image_height - j);
+        for i in 0..image_width {
+            let r = i as f64 / (image_width - 1) as f64;
+            let g = j as f64 / (image_height - 1) as f64;
+            let b = 0.0;
+
+            img.set_pixel(
+                i,
+                j,
+                [
+                    (r * 255.999) as u16,
+                    (g * 255.999) as u16,
+                    (b * 255.999) as u16,
+                ],
+            );
+        }
+    }
+
+    img.save_raw("render.ppm").expect("Failed to save image!");
 }
